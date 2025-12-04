59b77387721f2179bb421091c16467d0132c9aa4
Natalia <silviariego_nataliarodriguez>
2025-12-04 19:11:25 +0100

    migración de JAR a código fuente y configuración de Maven

--- src/test/java/es/iwt42/grupo6/cajaNegra/AlienTest.java
+++ src/test/java/es/iwt42/grupo6/cajaNegra/AlienTest.java
@@ -2,7 +2,6 @@ package es.iwt42.grupo6.cajaNegra;
 
 import main.Commons;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
--- src/test/java/es/iwt42/grupo6/cajaNegra/ShotTest.java
+++ src/test/java/es/iwt42/grupo6/cajaNegra/ShotTest.java
@@ -2,7 +2,7 @@ package es.iwt42.grupo6.cajaNegra;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import space_invaders.sprites.Shot;
